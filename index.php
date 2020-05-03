08de4fecb86528865a030de9842555f1aadc8899
theoGaillard <theo.g.31@free.fr>
2020-05-03 15:01:32 +0200

    Initiale Commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Index du projet GSB
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    José GIL <jgil@ac-nice.fr>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+
+require_once 'includes/fct.inc.php';
+require_once 'includes/class.pdogsb.inc.php';
+session_start();
+$pdo = PdoGsb::getPdoGsb();
+$estConnecte = estConnecte();
+$viisiteurConnecter = estVisiteurConnecter();
+$comptableConnecter = estComptableConnecter();
+require 'vues/v_entete.php';
+$uc = filter_input(INPUT_GET, 'uc', FILTER_SANITIZE_STRING);
+if ($uc && !$estConnecte) {
+    $uc = 'connexion';
+} elseif (empty($uc)) {
+    $uc = 'accueil';
+}
+switch ($uc) {
+case 'connexion':
+    include 'controleurs/c_connexion.php';
+    break;
+case 'accueil':
+    include 'controleurs/c_accueil.php';
+    break;
+case 'gererFrais':
+    include 'controleurs/c_gererFrais.php';
+    break;
+case 'etatFrais':
+    include 'controleurs/c_etatFrais.php';
+    break;
+case 'deconnexion':
+    include 'controleurs/c_deconnexion.php';
+    break;
+    
+case 'validerUnFrais':
+    include 'controleurs/c_validerUnFrais.php';
+    break;
+    
+case 'suivreUnPaiement':
+    // ok sa c'est good
+    include 'controleurs/c_suiviePaiement.php';
+    break;
+    
+}
+require 'vues/v_pied.php';
